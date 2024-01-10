d13e326e37b234dfd76ccd47178fa6fbe433453b
carlos eduardo <cadudevmaster@gmail.com>
2024-01-10 00:07:30 -0300

    chore: configure react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { SendReport } from "./pages/SendReport";
 import { Dash } from "./templates/Dash";
 import { ProviderTheme } from "./themes/Chakra/ProviderTheme";
+import { ReactQueryProvider } from './providers/ReactQuery'
 
 function App() {
   return (
    <ProviderTheme>
-    <Dash>
-      <SendReport/>
-    </Dash>
+    <ReactQueryProvider>
+      <Dash>
+        <SendReport/>
+      </Dash>
+    </ReactQueryProvider>
    </ProviderTheme>
   );
 }
