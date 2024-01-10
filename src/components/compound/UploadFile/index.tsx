8b43cfc11561fd7b1bda9716d2cf9855c7aa2bac
carlos eduardo <cadudevmaster@gmail.com>
2024-01-09 23:00:49 -0300

    feat: upload file with dragzone

--- src/components/compound/UploadFile/index.tsx
+++ src/components/compound/UploadFile/index.tsx
@@ -1,26 +1,54 @@
 import { Box, Stack, Text } from "@chakra-ui/react"
 import { Button } from "../../unique/Button"
+import { useCallback } from "react"
+import { useDropzone } from "react-dropzone"
 
-export const UploadFile = () => {
+type Props = {
+    onFileUpload: (file: File) => void,
+    allowExtensions: string[]
+}
+
+export const UploadFile = ({ onFileUpload, allowExtensions }: Props) => {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        if (acceptedFiles.length < 1) return;
+        
+        const isAllowExtension = allowExtensions.includes(acceptedFiles[0].type)
+        if (!isAllowExtension) {
+            alert(`Arquivos permitidos "${allowExtensions.join(',')}"`)
+            return;
+        }
+        
+        onFileUpload(acceptedFiles[0])
+    }, [onFileUpload, allowExtensions])
+
+    const { getRootProps, getInputProps, open, isDragActive } = useDropzone({ 
+        onDrop,
+        noClick: true,
+        noKeyboard: true 
+    });
+
     return (
-        <Box>
+        <Box {...getRootProps()}>
+            <input {...getInputProps()} />
             <Box
                 width='100%'
                 border='3px'
                 borderStyle="dashed"
                 borderRadius={20}
-                borderColor="copybase.general.whiteGray"
+                borderColor={isDragActive ? "copybase.general.purple" : "copybase.general.whiteGray"}
                 p={20}
+                opacity={isDragActive ? 0.5 : 1}
                 bg="copybase.general.whiteGrayClean"
+                transition="all 0.3s ease-out"
             >
                 <Stack spacing={4} align="center">
-                    <Text fontSize='3xl' color="copybase.general.purple">Drag and drop files here</Text>
-                    <Text fontSize='3xl' color="copybase.general.purple">-OR-</Text>
+                    <Text fontSize='3xl' color="copybase.general.purple">Arraste e solte o arquivo aqui</Text>
+                    <Text fontSize='3xl' color="copybase.general.purple">-OU-</Text>
                     <Button 
                         color="copybase.button" 
                         size="xbig"
                         padding="2rem"
-                        callback={()=>console.log('Helloworld')}
+                        callback={()=>open()}
                     >
                         Encontrar Arquivo
                     </Button>
--- src/pages/SendReport.tsx
+++ src/pages/SendReport.tsx
@@ -1,13 +1,17 @@
 import { Box, Grid, GridItem } from "@chakra-ui/react"
 import { UploadFile } from "../components/compound/UploadFile"
 import { ReportQueue } from "../components/compound/ReportQueue"
+import { useCallback } from "react"
 
 export const SendReport = () => {
+    const onUploadFile = useCallback((file: File) => {
+        console.log(file)
+    }, [])
     return (
         <Box>
             <Grid templateColumns='repeat(2, 1fr)' p={4} gap={4}>
                 <GridItem>
-                    <UploadFile/>
+                    <UploadFile allowExtensions={['text/csv']} onFileUpload={onUploadFile}/>
                 </GridItem>
                 <GridItem>
                     <ReportQueue/>
