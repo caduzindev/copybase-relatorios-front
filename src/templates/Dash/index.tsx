7f54d7717a817798cba5b59539e118d7fd52487f
carlos eduardo <cadudevmaster@gmail.com>
2024-01-21 14:25:16 -0300

    feat: alter logo

--- src/templates/Dash/index.tsx
+++ src/templates/Dash/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Image } from "@chakra-ui/react"
+import { Box, Image, Text } from "@chakra-ui/react"
 
 type Props = {
     children: React.ReactNode
@@ -6,8 +6,27 @@ type Props = {
 export const Dash = ({ children }: Props) => {
     return (
         <>
-            <Box width="100%" p={2} bg="copybase.general.purple">
-                <Image src="copybase_logo.svg" alt="logo"/>
+            <Box 
+                width="100%" 
+                p={2} 
+                bg="copybase.general.purple" 
+                display="flex"
+                alignItems="center"
+                columnGap={2}
+            >
+                <Image 
+                    src="informacao-financeira.png" alt="logo"
+                    height={30}
+                    background="#fff"
+                    borderRadius={30}
+                />
+                <Text
+                    color="copybase.general.white"
+                    fontWeight={700}
+                    fontSize={25}
+                    fontFamily="Sans serif"
+                    letterSpacing={1}
+                >Relatorios</Text>
             </Box>
             <Box width="100%">{children}</Box>        
         </>
